a7b44d69e2f514b04dba93ab4aedf81803558459
yangzhen10 <yangzhen10@staff.weibo.com>
2018-03-08 09:14:03 +0800

    add

---
+++ AComponent/AComponent/Class/PodClass.h
@@ -0,0 +1,13 @@
+//
+//  PodClass.h
+//  AComponent
+//
+//  Created by yangzhen10 on 2018/3/7.
+//  Copyright © 2018年 yangzhen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodClass : NSObject
+
+@end
---
+++ AComponent/AComponent/Class/PodClass.m
@@ -0,0 +1,13 @@
+//
+//  PodClass.m
+//  AComponent
+//
+//  Created by yangzhen10 on 2018/3/7.
+//  Copyright © 2018年 yangzhen. All rights reserved.
+//
+
+#import "PodClass.h"
+
+@implementation PodClass
+
+@end
